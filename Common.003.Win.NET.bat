21beae9b2691cbcdde92c0afc6428edbdb763145
HY-ZhengWei <hy.zhengwei@qq.com>
2018-03-07 12:07:19 +0800

    1.2.14

--- Common.003.Win.NET.bat
+++ Common.003.Win.NET.bat
@@ -2,6 +2,7 @@
 
 cd .\bin
 
+
 rd /s/q .\org\hy\common\net\junit
 rd /s/q .\org\hy\common\net\plugins
 
@@ -11,3 +12,12 @@ copy hy.common.net.jar ..
 del /q hy.common.net.jar
 cd ..
 
+
+
+
+
+cd .\src
+jar cvfm hy.common.net-sources.jar MANIFEST.MF META-INF org 
+copy hy.common.net-sources.jar ..
+del /q hy.common.net-sources.jar
+cd ..
--- Common.004.Mac.NET.sh
+++ Common.004.Mac.NET.sh
@@ -2,6 +2,7 @@
 
 cd ./bin
 
+
 rm -R ./org/hy/common/net/junit
 rm -R ./org/hy/common/net/plugins
 
@@ -11,3 +12,12 @@ cp hy.common.net.jar ..
 rm hy.common.net.jar
 cd ..
 
+
+
+
+
+cd ./src
+jar cvfm hy.common.net-sources.jar MANIFEST.MF META-INF org 
+cp hy.common.net-sources.jar ..
+rm hy.common.net-sources.jar
+cd ..
