65b43b9b57fd6cac5465d68fd8cc67ff88e2b83a
ZhengWei <HY.ZhengWei@qq.com>
2022-11-05 17:39:28 +0800

    3.0.2
    1. 添加：支持IDEA和Eclipse双编译器
    2. 添加：支持向私库发版 org
    3. 添加：支持向公共库发版 cn.openapis

--- Common.003.Win.NET.bat
+++ Common.003.Win.NET.bat
@@ -1,23 +1,29 @@
 
 
-cd .\bin
+del /Q hy.common.net.jar
+del /Q hy.common.net-sources.jar
+
+
+call mvn clean package
+cd .\target\classes
 
 
 rd /s/q .\org\hy\common\net\junit
-rd /s/q .\org\hy\common\net\plugins
 
-jar cvfm hy.common.net.jar MANIFEST.MF META-INF org
+jar cvfm hy.common.net.jar META-INF/MANIFEST.MF META-INF org
 
-copy hy.common.net.jar ..
+copy hy.common.net.jar ..\..
 del /q hy.common.net.jar
-cd ..
+cd ..\..
 
 
 
 
 
-cd .\src
-jar cvfm hy.common.net-sources.jar MANIFEST.MF META-INF org 
-copy hy.common.net-sources.jar ..
-del /q hy.common.net-sources.jar
-cd ..
+cd .\src\main\java
+xcopy /S ..\resources\* .
+jar cvfm hy.common.net-sources.jar META-INF\MANIFEST.MF META-INF org 
+copy hy.common.net-sources.jar ..\..\..
+del /Q hy.common.net-sources.jar
+rd /s/q META-INF
+cd ..\..\..
